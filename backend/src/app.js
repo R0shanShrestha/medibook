a2ce4197d7e75975a8fbd831f717bd86f9ca2471
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-08-25 20:33:56 +0545

    changes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
