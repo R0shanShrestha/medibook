0be0b7b80c2a5c8c5e907081094f650580133193
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-08-25 20:18:31 +0545

    changes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import adminRouter from "./routes/Admin.routes.js";
-import userRoutes from "./routes/user.routes.js";
 import doctorRoutes from "./routes/doctor.routes.js";
+import userRoutes from "./routes/User.routes.js";
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(
   })
 );
 // app.options("*", cors());
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // API endpoints
