04957d7f0cb5071f1ca68e944a09de8a58849a05
Roshan Shrestha <156121270+R0shanShrestha@users.noreply.github.com>
2025-08-25 17:59:57 +0545

    changes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:3000",
       "http://192.168.1.68:1235",
       "https://medibook-admin.vercel.app",
+      "https://medibook-roshan.vercel.app",
     ],
     credentials: true,
   })
